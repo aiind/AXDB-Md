5c9e74c1a61e3d8e4a8d05bf375e538eb5dd4be0
aiind <ainuddin.efbi@gmail.com>
2023-01-26 06:08:12 +0800

    AXDB

---
+++ config/configFile.js
@@ -0,0 +1,14 @@
+exports.info = {
+    prefix: '.',
+      prefix2: '#',
+      owner: {
+          nome: "4relial",
+          number: "Your Owner number",
+          numero: ["Your Owner Number"]
+      }, 
+      bot: {
+          name: "Megumi Katou",
+          number: "Your Bot Number"
+      },
+      grupo: ""
+  }
